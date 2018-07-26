5fd859d8388d39e27eac475e26d406637a1121eb
chirags98 <chirags1998@gmail.com>
2018-07-27 02:15:11 +0530

    Testing Fix

--- Relay Code/relay.py
+++ Relay Code/relay.py
@@ -5,13 +5,13 @@ current_state = 0;
 
 def relay_on(pin):
     global current_state
-    GPIO.output(pin, GPIO.HIGH)  # Turn relay on
+    GPIO.output(pin, GPIO.LOW)  # Turn relay on
     current_state = 1
 
 
 def relay_off(pin):
     global current_state
-    GPIO.output(pin, GPIO.LOW)  # Turn relay off
+    GPIO.output(pin, GPIO.HIGH)  # Turn relay off
     current_state = 0
 
 
