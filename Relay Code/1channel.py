0da439ca5ee7ef8321bf8a0d969bbdc952d06545
chirags98 <chirags1998@gmail.com>
2018-07-27 02:02:32 +0530

    Testing 4 channels

--- Relay Code/1channel.py
+++ Relay Code/1channel.py
@@ -2,21 +2,20 @@ import relay as relay
 import RPi.GPIO as GPIO
 import time
 
-channel1 = 16
-channel2 = 20
 channel3 = 21
-channel4 = 26
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(channel1, GPIO.OUT)
-GPIO.setup(channel2, GPIO.OUT)
+#Setup for channels
 GPIO.setup(channel3, GPIO.OUT)
-GPIO.setup(channel4, GPIO.OUT)
 
 if __name__ == '__main__':
     try:
+        # Turn Relay off
+        relay.relay_off(channel3)
+        time.sleep(1)
+
         #Change state of relay
         relay.change_relay_state(channel3)
         time.sleep(1)
@@ -32,20 +31,6 @@ if __name__ == '__main__':
         # Change state of relay
         relay.change_relay_state(channel3)
         time.sleep(1)
-
-        ######################################
-        # Turn Relays on
-        relay.relay_on(channel1)
-        relay.relay_on(channel2)
-        relay.relay_on(channel4)
-        time.sleep(1)
-
-        # Turn Relay off
-        relay.relay_off(channel1)
-        relay.relay_off(channel2)
-        relay.relay_off(channel4)
-        time.sleep(1)
-        ######################################
 
         GPIO.cleanup()  #Call at the very end of the program (before exiting the program)
     except KeyboardInterrupt:
